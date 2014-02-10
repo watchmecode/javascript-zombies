deb48d2bdb88dfc10ec6b9b2b44aabae0c355fd8
Derick Bailey <derickbailey@gmail.com>
2014-02-10 10:45:52 -0600

    shell of first demo

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index');
+};
+
+exports.viewDemo = function(req, res){
+  var pageName = req.params.pageName;
+  res.render("demos/" + pageName);
 };
